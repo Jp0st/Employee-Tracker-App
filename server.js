5ba887633e227288ab35f5beeff25f89ccd4582e
jp0st <jpost2010@gmail.com>
2023-11-27 16:12:38 -0500

    doesnt need to be heroku live :D

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 require('dotenv').config();
-const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const { mainModule } = require('process');
 const dotenv = require('dotenv');
 
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 3306,
-});
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    },
+    console.log(`Connected to the employee_db database.`)
+);
 
 db.connect((err) => {
     if (err) throw err;
