205f19f8918e677d27ec2df17b00165509f8754e
jp0st <jpost2010@gmail.com>
2023-11-27 15:58:41 -0500

    trying another solution

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_DATABASE 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 db.connect((err) => {
